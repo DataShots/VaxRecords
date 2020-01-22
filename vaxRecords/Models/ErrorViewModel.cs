37a6c30ffa3e0d0f02163ccdd5165b71716db62f
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2020-01-21 20:48:49 -0500

    added initial setup with Hello world

---
+++ vaxRecords/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vaxRecords.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
