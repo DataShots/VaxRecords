47b99cf1a8f7d6e9468dfe51dd21c088ebe89ab7
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2020-02-06 20:17:31 -0500

    made changes

--- Models/Vaccine.cs
+++ Models/Vaccine.cs
@@ -7,14 +7,18 @@ namespace VaxRecords.Models
 {
     public class Vaccine
     {
+        public int VaccineId { get; set; } //Primary keys
         public string VaccineName { get; set; }
         public string Description { get; set; }
         public float Dosage { get; set; }
         public string Place { get; set; }
 
+        //The Child property is a navigation property.A vaccine entity can be related to any number of Child entities.
+        public ICollection<Child> Childs { get; set; }
 
+        //Constructor
         public Vaccine(string vaccinename, string description, float dosage, string place)
-        { 
+        {
             VaccineName = vaccinename;
             Description = description;
             Dosage = dosage;
@@ -22,7 +26,4 @@ namespace VaxRecords.Models
 
         }
     }
-    
-    
-
 }
