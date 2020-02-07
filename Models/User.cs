34924bcdd31a561dc44cc6d9343d5044444d6f32
SimranPannu <53317578+SimranPannu@users.noreply.github.com>
2020-02-06 20:14:21 -0500

    made changes in user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,44 +7,35 @@ namespace VaxRecords.Models
 {
     public class User
     {
-            public string Name { get; set; }
-            public string Password { get; set; }
-
-            public string Email { get; set; }
-            public int UserId { get; set; }
-            private static int nextId = 1;
-
-            public User(string name,string password,string email) : this()
-            {
-                Name = name;
-                Password= password;
-                Email = email; 
-            }
-
-            public User()
-            {
-                UserId = nextId;
-                nextId++;
-            }
-
-            // override object.Equals
-            /*public override bool Equals(object obj)
-            {
-
-                if (obj == null || GetType() != obj.GetType())
-                {
-                    return false;
-                }
-
-                return UserId == (obj as User).UserId;
-            }*/
-
-            // override object.GetHashCode
-            public override int GetHashCode()
-            {
-                return UserId;
-            }
+        public int UserId { get; set; }//Primary key
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        private static int nextId = 1;
+        //Constructour for user class
+        public User(string name, string password, string email) : this()
+        {
+            Name = name;
+            Password = password;
+            Email = email;
         }
-    }
+        /*The Childs property is a navigation property. 
+        Navigation properties hold other entities that are 
+        related to this entity. In this case, the Childs
+        property of a User entity will hold all of the child
+        entities that are related to that user entity.*/
+        public ICollection<Child> Childs { get; set; }
+        
+        //Need to work on this function
+        public User()
+        {
+            UserId = nextId;
+            nextId++;
+        }
 
+    }
+}
+
+
+   
 
