702e8264286a80e8891c36aa1a7897147ab46604
lexgld <54690246+lexgld@users.noreply.github.com>
2020-02-11 14:04:13 -0500

    Add files via upload

---
+++ Models/VaccinationRecordContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace VaxRecords.Models
+{
+    public partial class VaccinationRecordContext : DbContext
+    {
+        public VaccinationRecordContext()
+        {
+        }
+
+        public VaccinationRecordContext(DbContextOptions<VaccinationRecordContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Vaccines> Vaccines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vaccines>(entity =>
+            {
+                entity.HasKey(e => e.VaccineId)
+                    .HasName("PK__Vaccines__45DC68890D9F78BA");
+
+                entity.Property(e => e.Clinician)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Date)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Method)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Notes)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Vaccine)
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Vaccines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VaxRecords.Models
+{
+    public partial class Vaccines
+    {
+        public int VaccineId { get; set; }
+        public string Vaccine { get; set; }
+        public string Date { get; set; }
+        public string Dose { get; set; }
+        public string Method { get; set; }
+        public string Clinician { get; set; }
+        public string Notes { get; set; }
+    }
+}
